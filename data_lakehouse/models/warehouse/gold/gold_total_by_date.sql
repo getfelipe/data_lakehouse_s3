082e201047e136b45e98f0d743694e35f7b3d0e3
getFelipe <felipesbsmachado@outlook.com>
2025-03-03 18:44:57 -0300

    finished first part

--- data_lakehouse/models/warehouse/gold/gold_total_by_date.sql
+++ data_lakehouse/models/warehouse/gold/gold_total_by_date.sql
@@ -4,6 +4,7 @@ WITH total_by_date AS (
         COUNT(order_date) as qntde_orders,
         ROUND(SUM(total_value::numeric), 2) AS total_date
     FROM {{ref("silver_orders")}}
+    WHERE order_status = 'invoiced'
     GROUP BY order_date
-    ORDER BY order_date
+    ORDER BY order_date DESC
 ) SELECT * FROM total_by_date
